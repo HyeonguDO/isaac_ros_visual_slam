7397dc2f4d50d5da0d2a44de6c45e7d1f3d118f4
HyeonguDO <dek0411@naver.com>
2024-11-14 20:02:12 +0900

    슬램 런치 파라미터 추가

--- isaac_ros_visual_slam/launch/visual_slam.launch.py
+++ isaac_ros_visual_slam/launch/visual_slam.launch.py
@@ -19,12 +19,12 @@ def generate_launch_description():
                     'enable_observations_view': True,
                     'map_frame': 'map',
                     'odom_frame': 'odom',
-                    # 'base_frame': 'zed'+'_camera_link',
+                    # 'base_frame': 'zed_camera_link',
                     'base_frame': 'base_link',
                     'input_imu_frame': 'zed'+'_imu_link',
                     'camera_optical_frames': ['zed'+'_left_camera_optical_frame',
                                               'zed'+'_right_camera_optical_frame'],
-                    'enable_imu_fusion': True,      
+                    'enable_imu_fusion': False,      
                     'gyro_noise_density': 0.0028,           # 제드 imu 기준 -> 추정치
                     'gyro_random_walk': 0.000019393,
                     'accel_noise_density': 0.0314,          # 제드 imu 기준 -> 추정치
@@ -34,7 +34,9 @@ def generate_launch_description():
                     'enable_ground_constraint_in_odometry': True,
                     'enable_ground_constraint_in_slam': True,
                     'multicam_mode': 2,                      # Multicamera mode: moderate (0), performance (1) or precision (2).   
-                    'enable_localization_n_mapping': True    # True면 맵 생성+localization, False면 localization만            
+                    'enable_localization_n_mapping': True,    # True면 맵 생성+localization, False면 localization만            
+                    'publish_map_to_odom_tf': True,
+                    'publish_odom_to_base_tf': True,
                     }],
         remappings=[('/visual_slam/image_0', '/zed/zed_node/left_gray/image_rect_gray'),
                     ('/visual_slam/camera_info_0', '/zed/zed_node/left_gray/camera_info'),
